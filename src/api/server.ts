d522978d4e17fd456da43812f407998a5ba26447
mkrass1988 <krassmatthew@gmail.com>
2023-09-14 10:25:53 -0400

    first commit

---
+++ src/api/server.ts
@@ -0,0 +1,79 @@
+const token = 'd3d42d3bb21d9a6d6f7f94c8a221cc31d9d2a62ff94a306a'
+
+export const server_calls = {
+    get: async () => { 
+        const response = await fetch(`https://car-inventory-s6cf.onrender.com/api/inventory`,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok){
+            throw new Error('Failed to fetch data from the server')
+        }
+
+        return await response.json()
+    },
+
+    create: async (data: any = {}) => {
+        const response = await fetch(`https://car-inventory-s6cf.onrender.com/api/cars`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to create new data on the server')
+        }
+
+        return await response.json()
+    },
+
+    update: async (id: string, data:any = {}) => {
+        const response = await fetch(`https://car-inventory-s6cf.onrender.com/api/inventory/${id}`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to update data on the server')
+        }
+
+        return await response.json()
+    },
+
+    delete: async (id: string) => {
+        const response = await fetch(`https://car-inventory-s6cf.onrender.com/api/inventory/${id}`,
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            },
+
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to delete data from the server')
+        }
+
+        return;
+    },
+}
